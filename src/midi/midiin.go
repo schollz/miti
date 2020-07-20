47f93e769a266f8ed15fb5bcdbc854f92cc6ffd0
Zack Scholl <zack.scholl@gmail.com>
2020-07-20 13:59:17 -0700

    add midi in

---
+++ src/midi/midiin.go
@@ -0,0 +1,56 @@
+package midi
+
+import (
+	"github.com/schollz/miti/src/log"
+	"github.com/schollz/miti/src/portmidi"
+)
+
+type Event struct {
+	MIDI       int
+	Instrument string
+}
+
+func ReadAll(finished chan bool) (events chan Event, err error) {
+	events = make(chan Event, 100)
+	err = portmidi.Initialize()
+	if err != nil {
+		return
+	}
+
+	numDevices := portmidi.CountDevices()
+	log.Debugf("found %d devices", numDevices)
+	done := make(chan bool)
+	for i := 0; i < numDevices; i++ {
+		di := portmidi.Info(portmidi.DeviceID(i))
+		if di.IsInputAvailable {
+			go func(id int, events chan Event, done chan bool) {
+				d := portmidi.Info(portmidi.DeviceID(i))
+				log.Debugf("reading from %s", d.Name)
+				in, err := portmidi.NewInputStream(portmidi.DeviceID(id), 1024)
+				if err != nil {
+					panic(err)
+				}
+				defer in.Close()
+
+				ch := in.Listen()
+				for {
+					select {
+					case event := <-ch:
+						log.Tracef("event: %+v", event)
+						events <- Event{int(event.Data1), d.Name}
+					case <-done:
+						log.Debug("closing midi read")
+						return
+					}
+				}
+			}(i, events, done)
+		}
+	}
+
+	<-finished
+	for i := 0; i < numDevices; i++ {
+		done <- true
+	}
+	portmidi.Terminate()
+	return
+}
--- src/record/record.go
+++ src/record/record.go
@@ -1,10 +1,24 @@
 package record
 
 import (
+	"os"
+	"os/signal"
+
 	"github.com/schollz/miti/src/log"
 )
 
 func Record() (err error) {
 	log.Debug("init recording")
+
+	// shutdown everything on Ctl+C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Debug(sig)
+			log.Info("shutting down")
+		}
+	}()
+
 	return
 }
