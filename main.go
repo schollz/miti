2a7ddf5a8250e6ce46e9469513e1861a25fe5c4b
Zack Scholl <zack.scholl@gmail.com>
2020-09-12 14:12:17 -0700

    bump 0.5.1

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func init() {
 	flag.StringVar(&flagRecord, "record", "", "record input to miti file")
 	flag.StringVar(&flagFile, "play", "", "play sequence from miti file")
 	if Version == "" {
-		Version = "v0.5.0-e06cefc"
+		Version = "v0.5.1-8fcdb33"
 	}
 }
 
